7d4d1a4a8a769840d2eb8e3505d7467234437e6f
Ezernex369 <pongpisut.rukt@sbac.ac.th>
2025-11-28 10:26:10 +0700

    bug code not working code i'm to fix this at home

---
+++ MainCalculation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class MenuCalculation  {
+
+    int Choice;
+    Scanner input = new Scanner(System.in);
+
+    void show_MenuCalculation(){
+        if (Choice ) {
+            
+        }
+    }
+
+            
+public class MainCalculation {
+    public static void main(String[] args) {
+        System.out.println("Pongpisut Ruktao");
+        System.out.println("Program Calculation");
+        System.out.println("1.(+)");
+        System.out.println("2.(-)");
+        System.out.println("3.(*) ");
+        System.out.println("4.(/)");
+        System.out.println("5.(%)");
+        System.out.print("input :");
+        Scanner input = new Scanner(System.in);
+        MenuCalculation S1 = new MenuCalculation();
+        S1.show_MenuCalculation();
+    }
+}
